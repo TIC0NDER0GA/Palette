818e64881c77a61ca1956f5aaae8afb2c63f7913
dey60704 <dey60704@uga.edu>
2022-02-19 16:11:15 -0500

    Add project files.

---
+++ Palette/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Palette
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+
+            count = count + 69;
+            ((Button)sender).Text = $"You aroused me {count} times";
+        }
+
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Sign_Up());
+        }
+    }
+}
---
+++ Palette/Sign_Up.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Palette
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Sign_Up : ContentPage
+    {
+        public Sign_Up()
+        {
+            InitializeComponent();
+        }
+
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainPage());
+        }
+    }
+}
